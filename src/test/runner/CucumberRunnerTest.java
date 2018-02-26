d36a52d01552ee32105706037150fc96b40baaf2
Rajiv Bhati <rajivbhati12@gmail.com>
2018-02-26 18:21:49 -0500

    Add feature heartbeat test for Google custom api

---
+++ src/test/runner/CucumberRunnerTest.java
@@ -0,0 +1,23 @@
+package test.runner;
+
+/**
+ * Created by rajivbhati on 2/26/18.
+ */
+
+
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "src/test/services",
+        format = {"pretty","html:target","json:target/WebServiceBehaviourTest.json"},
+        tags = {"@heartbeat"},
+        monochrome = true
+)
+public class CucumberRunnerTest {
+}
+
