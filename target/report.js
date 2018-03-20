e5aa9d9f54def9f844a55698c279eaa59dbed8f5
Rajiv Bhati <rajivbhati12@gmail.com>
2018-03-20 17:55:58 -0400

    Add git Ignore & remove target folder

--- target/report.js
+++ target/report.js
@@ -1,26 +1,26 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("GoogleMapsApi.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("GoogleCustomSearchApi.feature");
 formatter.feature({
   "line": 2,
-  "name": "The Google Maps web services are a collection of HTTP interfaces to Google services",
-  "description": "providing geographic data for your maps applications",
-  "id": "the-google-maps-web-services-are-a-collection-of-http-interfaces-to-google-services",
+  "name": "The JSON/Atom Custom Search API lets you develop websites and applications to retrieve and",
+  "description": "display search results from Google Custom Search programmatically.",
+  "id": "the-json/atom-custom-search-api-lets-you-develop-websites-and-applications-to-retrieve-and",
   "keyword": "Feature",
   "tags": [
     {
       "line": 1,
-      "name": "@Service-GoogleMapsApi"
+      "name": "@Service-GoogleCustomSearchApi"
     }
   ]
 });
 formatter.before({
-  "duration": 451719310,
+  "duration": 556443292,
   "status": "passed"
 });
 formatter.scenario({
   "line": 6,
-  "name": "To verify access to Google Maps Api",
+  "name": "To verify access to Google api",
   "description": "",
-  "id": "the-google-maps-web-services-are-a-collection-of-http-interfaces-to-google-services;to-verify-access-to-google-maps-api",
+  "id": "the-json/atom-custom-search-api-lets-you-develop-websites-and-applications-to-retrieve-and;to-verify-access-to-google-api",
   "type": "scenario",
   "keyword": "Scenario",
   "tags": [
@@ -49,26 +49,137 @@ formatter.match({
   "location": "CommonImpl.setServiceApi()"
 });
 formatter.result({
-  "duration": 82058207,
+  "duration": 103282928,
   "status": "passed"
 });
 formatter.match({
   "location": "CommonImpl.initiateCall()"
 });
 formatter.result({
-  "duration": 1390343299,
+  "duration": 561181750,
   "status": "passed"
 });
 formatter.match({
   "location": "CommonImpl.verifySuccessfulResponse()"
 });
 formatter.result({
-  "duration": 1730922,
-  "error_message": "java.lang.AssertionError: expected:\u003c404\u003e but was:\u003c200\u003e\n\tat org.junit.Assert.fail(Assert.java:88)\n\tat org.junit.Assert.failNotEquals(Assert.java:834)\n\tat org.junit.Assert.assertEquals(Assert.java:118)\n\tat org.junit.Assert.assertEquals(Assert.java:144)\n\tat test.java.steps.CommonImpl.verifySuccessfulResponse(CommonImpl.java:37)\n\tat ✽.Then I should get a success in response(GoogleMapsApi.feature:9)\n",
-  "status": "failed"
+  "duration": 1513691,
+  "status": "passed"
 });
 formatter.after({
-  "duration": 29838,
+  "duration": 17278,
+  "status": "passed"
+});
+formatter.uri("GoogleMapsApi.feature");
+formatter.feature({
+  "comments": [
+    {
+      "line": 1,
+      "value": "# Setup:"
+    },
+    {
+      "line": 2,
+      "value": "# a. Need Google Api Key"
+    },
+    {
+      "line": 3,
+      "value": "# b. Enable GoogleMapsApi for Google Api Key via https://console.developers.google.com/apis/api/directions_backend?project\u003d_"
+    }
+  ],
+  "line": 6,
+  "name": "The Google Maps web services are a collection of HTTP interfaces to Google services",
+  "description": "providing geographic data for your maps applications",
+  "id": "the-google-maps-web-services-are-a-collection-of-http-interfaces-to-google-services",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "line": 5,
+      "name": "@Service-GoogleMapsApi"
+    }
+  ]
+});
+formatter.before({
+  "duration": 46751648,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 11,
+  "name": "User should be able to get driving directions between two location",
+  "description": "",
+  "id": "the-google-maps-web-services-are-a-collection-of-http-interfaces-to-google-services;user-should-be-able-to-get-driving-directions-between-two-location",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 9,
+      "name": "@Smoke"
+    },
+    {
+      "line": 10,
+      "name": "@Operation-directions"
+    }
+  ]
+});
+formatter.step({
+  "line": 12,
+  "name": "the service api",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 13,
+  "name": "from Disneyland to Universal Studios Hollywood",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 14,
+  "name": "I initiate the call for directions",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 15,
+  "name": "I should get a success in response",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "CommonImpl.setServiceApi()"
+});
+formatter.result({
+  "duration": 159579,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Disneyland",
+      "offset": 5
+    },
+    {
+      "val": "Universal Studios Hollywood",
+      "offset": 19
+    }
+  ],
+  "location": "GoogleMapsApi.setSourceDestination(String,String)"
+});
+formatter.result({
+  "duration": 5112504,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CommonImpl.initiateCall()"
+});
+formatter.result({
+  "duration": 1828836346,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CommonImpl.verifySuccessfulResponse()"
+});
+formatter.result({
+  "duration": 16686,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 6657,
   "status": "passed"
 });
 });
